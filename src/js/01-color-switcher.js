349f6af8906b4050ebe08b7deed0642f8ff458f8
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2022-12-05 08:00:40 +0200

    complete 2 hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
+let timerId = null;
+
+function onClickStart(e) {
+    timerId = setInterval((e) => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+}
+
+function onClickStop(e) {
+    clearInterval(timerId);
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+}
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop)
---
+++ src/js/02-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from "notiflix";
+
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+const startBtn = document.querySelector('button[data-start]')
+
+startBtn.setAttribute('disabled', true);
+const DELAY = 1000;
+
+let dates = null
+
+startBtn.addEventListener('click' , startTimer)
+
+
+flatpickr("#datetime-picker", {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        dates = selectedDates[0];
+        inputDates(dates)
+    },
+});
+
+
+function inputDates(date) {
+    if (date >= Date.now()) {
+        startBtn.removeAttribute('disabled')
+    } else {
+        Notify.failure('Change dates')
+    }
+}
+
+function startTimer() {
+    startBtn.setAttribute('disabled', true);
+
+
+    const timerId = setInterval(() => {
+        const timerValue = dates - Date.now();
+        const { days, hours, minutes, seconds } = convertMs(timerValue);
+        daysEl.textContent = days;
+        hoursEl.textContent = hours;
+        minutesEl.textContent = minutes;
+        secondsEl.textContent = seconds
+    
+        if (timerValue < DELAY) {
+          clearInterval(timerId);
+          Notify.success(`${dates} is now`);
+        }
+      }, DELAY);
+      //   console.log(inputDates);
+    
+    
+    
+}
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
