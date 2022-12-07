fd583dca3faf781b453d7d3a054d659512c51af6
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2022-12-07 09:58:27 +0200

    complete hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from "notiflix";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  
+  let intCount = Number(e.target.elements.amount.value);
+  let intStep = Number(e.target.elements.step.value);
+  let intDelay = Number(e.target.elements.delay.value);
+
+  for (let i = 0; i <= intCount; i += 1) {
+    createPromise(i, intDelay).then(onSuccess).catch(onError)
+    intDelay += intStep
+  }
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        return resolve({ position, delay })
+      } else {
+        return reject({ position, delay })
+      }
+    }, delay);
   }
+  )
+  
+};
+
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+};
+
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
 }
+
+
